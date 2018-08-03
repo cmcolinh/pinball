d01d522d67e7a813218840ac7be0e71f91795682
colin <H2597@token.co.jp>
2018-08-03 15:40:18 +0900

    Minor update to configuration

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Pinball
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.active_record.table_name_prefix = "tbl"
+    config.active_record.pluralize_table_names = false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
